b3eaa115a47967a11ea25c93360b6c9304ccc693
vince-CV <vince@arylla.com>
2020-04-05 17:56:07 -0400

    upload a package to pypi

---
+++ Software_Engineering/software_package/5_upload_to_pypi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udc_distributions',
+      version='0.2',
+      description='Vince UDC Gaussian distributions Practice',
+      packages=['udc_distributions'],
+      zip_safe=False)
