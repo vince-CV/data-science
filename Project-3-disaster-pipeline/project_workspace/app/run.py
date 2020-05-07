ca065f7ae5c13daed3f355774274990c1d24d2df
vince-CV <vince@arylla.com>
2020-05-06 22:15:36 -0400

    graph visual

--- Project-3-disaster-pipeline/project_workspace/app/run.py
+++ Project-3-disaster-pipeline/project_workspace/app/run.py
@@ -10,11 +10,19 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+from collections import Counter
 
 
 app = Flask(__name__)
 
 def tokenize(text):
+    """
+    Tokenizes a given text.
+    Args:
+    text: text
+    Returns:
+    array of clean tokens
+    """
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -43,9 +51,31 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    # Message counts for different categories
+    cate_counts_df = df.iloc[:, 4:].sum().sort_values(ascending=False)
+    cate_counts = list(cate_counts_df)
+    cate_names = list(cate_counts_df.index)
+
+    # Top keywords in Social Media in percentages
+    social_media_messages = ' '.join(df[df['genre'] == 'social']['message'])
+    social_media_tokens = tokenize(social_media_messages)
+    social_media_wrd_counter = Counter(social_media_tokens).most_common()
+    social_media_wrd_cnt = [i[1] for i in social_media_wrd_counter]
+    social_media_wrd_pct = [i/sum(social_media_wrd_cnt) *100 for i in social_media_wrd_cnt]
+    social_media_wrds = [i[0] for i in social_media_wrd_counter]
+
+    # Top keywords in Direct in percentages
+    direct_messages = ' '.join(df[df['genre'] == 'direct']['message'])
+    direct_tokens = tokenize(direct_messages)
+    direct_wrd_counter = Counter(direct_tokens).most_common()
+    direct_wrd_cnt = [i[1] for i in direct_wrd_counter]
+    direct_wrd_pct = [i/sum(direct_wrd_cnt) * 100 for i in direct_wrd_cnt]
+    direct_wrds = [i[0] for i in direct_wrd_counter]
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+
     graphs = [
+    # Histogram of the message genere
         {
             'data': [
                 Bar(
@@ -63,7 +93,66 @@ def index():
                     'title': "Genre"
                 }
             }
-        }
+        },
+        # histogram of social media messages top 20 keywords 
+        {
+            'data': [
+                    Bar(
+                        x=social_media_wrds[:20],
+                        y=social_media_wrd_pct[:20]
+                                    )
+            ],
+
+            'layout':{
+                'title': "Top 20 Keywords in Social-Media Messages",
+                'xaxis': {'tickangle':60
+                },
+                'yaxis': {
+                    'title': "% Total Social Media Messages"    
+                }
+            }
+        }, 
+
+        # histogram of direct messages top 20 keywords 
+        {
+            'data': [
+                    Bar(
+                        x=direct_wrds[:20],
+                        y=direct_wrd_pct[:20]
+                                    )
+            ],
+
+            'layout':{
+                'title': "Top 20 Keywords in Direct Messages",
+                'xaxis': {'tickangle':60
+                },
+                'yaxis': {
+                    'title': "% Total Direct Messages"    
+                }
+            }
+        }, 
+
+
+
+        # histogram of messages categories distributions
+        {
+            'data': [
+                    Bar(
+                        x=cate_names,
+                        y=cate_counts
+                                    )
+            ],
+
+            'layout':{
+                'title': "Distribution of Message Categories",
+                'xaxis': {'tickangle':60
+                },
+                'yaxis': {
+                    'title': "count"    
+                }
+            }
+        },     
+
     ]
     
     # encode plotly graphs in JSON
@@ -72,6 +161,7 @@ def index():
     
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
 
 
 # web page that handles user query and displays model results
